1181df6a0154779431979138af2baf41624a970a
flavio088 <flavio088gomes@gmail>
2024-02-28 23:23:45 -0300

    Obtendo elementos HTML do DOM

---
+++ 03-obtendo-elementos/index.js
@@ -0,0 +1,12 @@
+function show() {
+    const contactList = document.getElementById('contact-list')
+    console.log(contactList)
+
+    const listElements = document.getElementsByTagName('li')
+    console.log(listElements)
+
+    const contactInputs = document.getElementsByClassName('contact-input')
+    console.log(contactInputs)
+
+    const contacts = document.querySelectorAll('#contact-input > li > label')
+}
